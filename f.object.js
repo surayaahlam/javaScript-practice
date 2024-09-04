8f6adbf1e703d2138e26e47865b78e0c8a070593
surayaahlam <surayaahlam@gmail.com>
2024-09-05 01:31:49 +0600

    added object practice

--- f.object.js
+++ f.object.js
@@ -2,7 +2,6 @@
  * Task-1
 Access the `golden rod` color value in output.
 */
-
 const colors = {
     red: "#ff0000",
     green: "#00ff00",
@@ -10,22 +9,27 @@ const colors = {
     "golden rod": '#daa520'
 };
 
+// Ans:
+console.log(colors["golden rod"]);
+
 /***
  * Task-2
 For this object below add a property named `passenger capacity` with value 5
 */
-
 const car = {
     make: "Toyota",
     model: "Corolla",
     year: 2020
 };
 
+// Ans:
+car[`passenger capacity`] = 5;
+console.log(car);
+
 /***
  * Task-3
 Display the physics marks as output.
 */
-
 const student = {
     name: "Hamim Sakep",
     id: 5421,
@@ -36,17 +40,22 @@ const student = {
     }
 };
 
+// Ans:
+console.log(student.physics.marks);
+
 /***
  * Task-4
 Count the `number of properties`. Output: 4
 */
-
 let person = {
     name: 'Ariana Grande',
     age: 21,
     city: 'Gaibandha',
     isStudent: true
 };
+
+// Ans:
+console.log(Object.keys(person).length);
 
 /***
  * Task-5 (Hard)
@@ -65,6 +74,11 @@ let myObject = {
     city: 'Example City',
     isStudent: true
 };
+
+// Ans:
+for (const key in myObject){
+    console.log("key:", key, "| type:", typeof myObject[key]);
+}
 
 
 
