0f0024537ab848251f7cea7b41acfa7ce870ba5c
surayaahlam <surayaahlam@gmail.com>
2024-09-05 02:18:33 +0600

    added array-looping practice(edited)

--- c.2.array.js
+++ c.2.array.js
@@ -22,9 +22,9 @@ Write a JavaScript code to get the even numbers from an array using any looping 
 const numbers = [12, 98, 5, 41, 23, 78, 46];
 
 const evenNumbers = [];
-for (i = 0; i < numbers.length; i++){
-    if (numbers[i] % 2 === 0){
-        evenNumbers.push(numbers[i]);
+for (num of numbers){
+    if (num % 2 === 0){
+        evenNumbers.push(num);
     }
 }
 console.log(evenNumbers);
